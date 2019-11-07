1f8b4f9893d8ad865ec44be189150d5c13a9c257
Bryan Sullivan <Bsully505@yahoo.com>
2019-11-07 15:34:05 -0500

    finished pause in hardmode

--- WaveGameB4Master-master/src/mainGame/KeyInput.java
+++ WaveGameB4Master-master/src/mainGame/KeyInput.java
@@ -121,9 +121,9 @@ public class KeyInput extends KeyAdapter {
 				
 				
 			} else if (game.gameState == STATE.Pause || game.gameState == STATE.PauseH1 || game.gameState == STATE.PauseH2 || game.gameState == STATE.PauseH3 || game.gameState ==STATE.PauseShop){
-				//game.socket.emit("getBoard");
 				if(HModeflag){
 					game.gameState = STATE.GameHard;
+					HModeflag = false;
 				}
 				else{
 				game.gameState = STATE.Game;
